9cb1f10af8e7254d9027d7a32986298aa3a165f0
munozjon <munozjon98@gmail.com>
2024-06-14 08:59:19 -0400

    Markers initialized, radius based on magnitude, color based on depth initialized

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,30 +1,60 @@
-let basemap = L.tileLayer(
-    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
-    {
-      attribution:
-        'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+// Set base tileLayer
+var baseMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+// Create the map
 let myMap = L.map( "map", {
-    center: [45,-75],
-    zoom: 6
+    center: [40,-105],
+    zoom: 4
 });
 
-basemap.addTo(myMap);
+baseMap.addTo(myMap);
 
+// Get the color based on earthquake depth -> feature.geometry.coordinates[2]
+function getColor (depth) {
+    if (depth > 10) {
+        return "blue"
+    }
+    else {
+        return "green"
+    }
+};
+
+// Get the radius based on magnitude -> feature.properties.mag
+function getRadius (magnitude) {
+
+    if (magnitude == 0) {
+        magnitude += 1
+    }
+
+    return magnitude * 5
+};
+
+// Set the url to the geoJson file
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+// Make a call to the geoJson url
 d3.json(url).then(function (data) {
    
     // Add a GeoJSON layer to the map after loading the file
     let geoJson = L.geoJson(data, {
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(`Magnitude: ${feature.properties.mag}`);
+            layer.bindPopup(`<h3>Magnitude: ${feature.properties.mag}</h3> <hr> <h3>Depth: ${feature.geometry.coordinates[2]}</h3>`);
         },
     
-    // Turn each feature into a circleMarker on the map
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng);
+        // Turn each feature into a circleMarker on the map
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: getRadius(feature.properties.mag),
+                fillColor: getColor(feature.geometry.coordinates[2]),
+                color: "#000000",
+                opacity: 1,
+                fillOpacity: 1,
+                stroke: true,
+                weight: 0.5
+            });
         }
 
     // Add the legend (look at the activities from day 2 and day 3)
